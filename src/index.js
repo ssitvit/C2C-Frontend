27fb70172216c22d4db5547f763d3af6af828d89
Nanduri Jayant Vishnu <nandurijayant.vishnu2020@vitstudent.ac.in>
2022-09-25 05:15:30 +0530

    changed test time

--- src/index.js
+++ src/index.js
@@ -6,23 +6,23 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Box } from "@mui/material";
 
-document.onkeydown = function(e) {
-  if(e.keyCode === 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
-     return false;
-  }
-}
+// document.onkeydown = function(e) {
+//   if(e.keyCode === 123) {
+//      return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
+//      return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
+//      return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
+//      return false;
+//   }
+//   if(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
+//      return false;
+//   }
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
