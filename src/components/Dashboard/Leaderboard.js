83d3cecf67037bb1f2f87c233719be020a91709a
Nanduri Jayant Vishnu <nandurijayant.vishnu2020@vitstudent.ac.in>
2022-10-01 14:23:52 +0530

    results declared

--- src/components/Dashboard/Leaderboard.js
+++ src/components/Dashboard/Leaderboard.js
@@ -4,7 +4,7 @@ import useWindowSize from "react-use/lib/useWindowSize";
 import Confetti from "react-confetti";
 function Leaderboard(props) {
   const [array, setArray] = useState([]);
-  const {width,height} = useWindowSize();
+  const { width, height } = useWindowSize();
   useEffect(() => {
     let url = `https://${process.env.REACT_APP_BASE_URL}/user/getScoreSort`;
     fetch(url, {
@@ -12,7 +12,7 @@ function Leaderboard(props) {
       credentials: "include",
       cache: "no-cache",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ round: props.round==='0'?3:props.round }),
+      body: JSON.stringify({ round: props.round === "0" ? 3 : props.round }),
     })
       .then((res) => res.json())
       .then((data) => {
@@ -20,11 +20,13 @@ function Leaderboard(props) {
       });
   }, [props.round]);
   return (
-    <div style={{ fontFamily: "Audiowide" }}>
+    <Stack spacing={4} style={{ fontFamily: "Audiowide" }}>
       {/* {new Date().getTime()>=new Date(process.env.REACT_APP_RESULT3
                           ).getTime()&&<Confetti width={width} height={height} tweenDuration={2000}/>} */}
-      {/* {props.round==='0'&&<Confetti width={width} height={height} tweenDuration={5000}/>} */}
-      {array &&
+      {props.round === "0" && (
+        <Confetti width={width} height={height} tweenDuration={5000} />
+      )}
+      {/* {array &&
         (array.length === 0
           ? "Nothing to show yet :/"
           : array.slice(0, 10).map((user, index) => {
@@ -59,8 +61,20 @@ function Leaderboard(props) {
                   </Stack>
                 );
               }
-            }))}
-    </div>
+            }))} */}
+      <Stack direction="row">
+        <div style={{ textAlign: "center", width: "100px" }}>1.</div>
+        <div style={{ width: "100%" }}>Likhit Ajeesh</div>
+      </Stack>
+      <Stack direction="row">
+        <div style={{ textAlign: "center", width: "100px" }}>2.</div>
+        <div style={{ width: "100%" }}>Aryan Shah</div>
+      </Stack>
+      <Stack direction="row">
+        <div style={{ textAlign: "center", width: "100px" }}>3.</div>
+        <div style={{ width: "100%" }}>Yashaswini Shivathaya</div>
+      </Stack>
+    </Stack>
   );
 }
 
